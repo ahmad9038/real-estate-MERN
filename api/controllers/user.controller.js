b0ca3f560cb655120d74e948926fac8da62cc62e
ahmadShahzad <ahmadshahzad.9038@gmail.com>
2024-06-14 12:28:36 +0500

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,37 @@
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({
     message: "helo world",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(403, "Unothorized"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    // separate password so that it will not show in response
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
