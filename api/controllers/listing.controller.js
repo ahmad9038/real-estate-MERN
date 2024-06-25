71489fdb3fa164d95686fc343f361d297e85de09
ahmadShahzad <ahmadshahzad.9038@gmail.com>
2024-06-25 20:50:39 +0500

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -48,3 +48,16 @@ export const updateListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "listing not fount!"));
+    }
+
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
