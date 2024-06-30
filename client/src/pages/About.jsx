80b62e0bf1282841c85944195cbcb23c3d5ca37d
ahmadShahzad <ahmadshahzad.9038@gmail.com>
2024-06-30 17:04:41 +0500

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
-const About = () => {
-  return <div></div>;
-};
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About AhmadEstate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        AhmadEstate is a luxurious oasis in the heart of the city, where luxury
+        meets serenity and elegance blends with sophistication. This prestigious
+        residential project is nestled in the heart of Pakistan's vibrant city,
+        sprawling over acres of lush green landscapes, providing a tranquil
+        retreat from the hustle and bustle of urban life.
+      </p>
+      <p className="mb-4 text-slate-700">
+        The grand entrance is a sight to behold, with impeccable architecture
+        inspired by the region's rich cultural heritage. The sleek, modern
+        design is complemented by opulent finishes, setting the tone for an
+        unparalleled living experience. The lavish villas are spacious and
+        exquisitely designed, with private gardens, rooftop terraces, and
+        breathtaking city views.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Residents can indulge in state-of-the-art facilities, including a fully
+        equipped gym, yoga studio, infinity pool, and spa, catering to their
+        wellness and relaxation needs. The exclusive clubhouse is a hub for
+        social gatherings, featuring a restaurant, lounge, and entertainment
+        areas. The secure and gated community ensures the safety and privacy of
+        residents, with 24/7 security and smart home technology for seamless
+        control and convenience.
+      </p>
 
-export default About;
+      <p>
+        <span className=" font-bold">CONTACT ME: </span>
+        <span>(ahmadshahzad.9038@gmail.com)</span>
+      </p>
+    </div>
+  );
+}
